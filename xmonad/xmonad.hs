d089b373625d91cf6a5bedfaafbb9836947e6528
Ken Micklas <kmicklas@gmail.com>
2014-05-20 17:17:52 -0400

    Add keybinding to lock screen

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,3 +1,6 @@
 import XMonad
- 
-main = xmonad defaultConfig { modMask = mod4Mask }
+import XMonad.Util.Run
+import XMonad.Util.EZConfig
+
+main = xmonad $ defaultConfig { modMask = mod4Mask }
+  `additionalKeys` [((mod4Mask, xK_End), safeSpawn "gnome-screensaver-command" ["-l"])]
