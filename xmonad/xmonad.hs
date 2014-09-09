f591f708e1133e6004b2838887c9626b7f8624d1
Ken Micklas <kmicklas@gmail.com>
2014-09-08 21:46:36 -0400

    Add keybindings to xmonad

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -2,5 +2,10 @@ import XMonad
 import XMonad.Util.Run
 import XMonad.Util.EZConfig
 
-main = xmonad $ defaultConfig { modMask = mod4Mask }
-  `additionalKeys` [((mod4Mask, xK_End), safeSpawn "gnome-screensaver-command" ["-l"])]
+main = xmonad $ defaultConfig
+         { modMask = mod4Mask 
+         , terminal = "gnome-terminal"
+         } `additionalKeys`
+         [ ((mod4Mask, xK_End), safeSpawn "xlock" [])
+         , ((mod4Mask, xK_Page_Down), safeSpawn "systemctl" ["suspend"])
+         ]
