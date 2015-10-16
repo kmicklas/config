34d91f82c256930110c10a455bcc5b0fee967bb8
Ken Micklas <kmicklas@gmail.com>
2015-10-16 00:54:36 -0400

    Smart borders for xmonad

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,9 +1,11 @@
 import XMonad
 import XMonad.Util.Run
 import XMonad.Util.EZConfig
+import XMonad.Layout.NoBorders
 
 main = xmonad $ defaultConfig
          { modMask = mod4Mask 
+         , layoutHook = smartBorders $ layoutHook defaultConfig
          , terminal = "gnome-terminal"
          } `additionalKeys`
          [ ((mod4Mask, xK_End), safeSpawn "xlock" [])
