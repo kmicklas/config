213e7cab15012bcde7cfcc317b7cc048c19c8ea0
Ken Micklas <kmicklas@gmail.com>
2016-10-24 14:04:09 -0400

    Clean up prompt and put status first

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -20,20 +20,13 @@ function fish_prompt --description 'Write out the prompt'
     set __fish_color_status (set_color -o red)
   end
 
-  switch $USER
-  case root
-    if not set -q __fish_prompt_cwd
-      if set -q fish_color_cwd_root
-        set -g __fish_prompt_cwd (set_color $fish_color_cwd_root)
-      else
-        set -g __fish_prompt_cwd (set_color $fish_color_cwd)
-      end
-    end
-    printf '%s@%s %s%s%s# ' $USER $__fish_prompt_hostname "$__fish_prompt_cwd" (prompt_pwd) "$__fish_prompt_normal"
-  case '*'
-    if not set -q __fish_prompt_cwd
-      set -g __fish_prompt_cwd (set_color $fish_color_cwd)
-    end
-    printf '[%s] %s%s@%s %s%s %s(%s)%s \f\r> ' (date "+%H:%M:%S") "$__fish_color_blue" $USER $__fish_prompt_hostname "$__fish_prompt_cwd" (pwd) "$__fish_color_status" "$stat" "$__fish_prompt_normal"
-  end
+  printf '%s(%s)%s ' "$__fish_color_status" "$stat" "$__fish_prompt_normal"
+
+  printf '[%s] ' (date "+%H:%M:%S")
+
+  printf '%s%s@%s%s ' "$__fish_color_blue" "$USER" "$__fish_prompt_hostname" "$__fish_prompt_normal"
+
+  printf '%s ' (pwd)
+
+  printf '\f\r> '
 end
