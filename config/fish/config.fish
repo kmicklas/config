327b70c01d0df173d12f045d6526223032d75b60
Ken Micklas <kmicklas@gmail.com>
2017-02-07 15:35:50 -0500

    Make fish source local config/functions if they exist

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,3 +1,9 @@
 set fish_user_paths ~/bin
 set -e fish_greeting
 set fish_prompt_pwd_dir_length 0
+if test -f ~/config-local/config/fish/config.fish
+  . ~/config-local/config/fish/config.fish
+end
+for func in ~/config-local/config/fish/functions/*.fish
+  . $func
+end
