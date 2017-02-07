01f7988dd1bf4ba53128bf85f3977cb67d88914d
Ken Micklas <kmicklas@gmail.com>
2017-02-07 16:33:53 -0500

    Update to new fish source syntax

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,8 +2,8 @@ set fish_user_paths ~/bin
 set -e fish_greeting
 set fish_prompt_pwd_dir_length 0
 if test -f ~/config-local/config/fish/config.fish
-  . ~/config-local/config/fish/config.fish
+  source ~/config-local/config/fish/config.fish
 end
 for func in ~/config-local/config/fish/functions/*.fish
-  . $func
+  source $func
 end
