2ff3d1b1aa65fceddb49e25f33c70175675c6b1b
Ken Micklas <kmicklas@gmail.com>
2017-02-08 18:14:39 -0500

    Fix notify status indicator

--- config/fish/functions/notify.fish
+++ config/fish/functions/notify.fish
@@ -1,4 +1,5 @@
 function notify
 	eval "$argv"
-	curl -s -F token=(cat ~/.config/pushover/token) -F user=(cat ~/.config/pushover/user) -F title=Terminal -F "message=$argv" https://api.pushover.net/1/messages.json > /dev/null 2>&1
+	set -l stat $status
+	curl -s -F token=(cat ~/.config/pushover/token) -F user=(cat ~/.config/pushover/user) -F title=Terminal -F "message=($stat) $argv" https://api.pushover.net/1/messages.json > /dev/null 2>&1
 end
