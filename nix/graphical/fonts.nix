b2550b75a0c96d05e1123dfec4d08ace02d0af30
Ken Micklas <kmicklas@gmail.com>
2017-02-22 20:46:47 -0800

    Fix fonts.nix

--- nix/graphical/fonts.nix
+++ nix/graphical/fonts.nix
@@ -1,35 +1,15 @@
-{ config, pkgs, ... }:
-
-{
-  hardware.pulseaudio.enable = true;
-
-  environment.systemPackages = (import ./packages.nix) pkgs;
-
-  fonts = {
-    enableFontDir = true;
-    enableGhostscriptFonts = true;
-    fonts = with pkgs; [
-      corefonts
-      ubuntu_font_family
-      unifont
-      google-fonts
-      font-droid
-      source-han-sans-japanese
-      source-han-sans-korean
-      source-han-sans-simplified-chinese
-      source-han-sans-traditional-chinese
-    ];
-  };
-
-  nixpkgs.config.firefox.enableAdobeFlash = true;
-
-  services.printing.enable = true;
-
-  services.xserver.enable = true;
-  services.xserver.layout = "us";
-
-  services.xserver.windowManager.xmonad.enable = true;
-  services.xserver.windowManager.xmonad.enableContribAndExtras = true;
-  services.xserver.windowManager.default = "xmonad";
-  services.xserver.desktopManager.xterm.enable = false;
+pkgs: {
+  enableFontDir = true;
+  enableGhostscriptFonts = true;
+  fonts = with pkgs; [
+    corefonts
+    ubuntu_font_family
+    unifont
+    google-fonts
+    font-droid
+    source-han-sans-japanese
+    source-han-sans-korean
+    source-han-sans-simplified-chinese
+    source-han-sans-traditional-chinese
+  ];
 }
