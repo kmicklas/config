29adb01f9daef8becc7c40fad9d925a09b0a8d80
Ken Micklas <kmicklas@gmail.com>
2017-11-11 19:54:01 -0500

    Fix fish greeting removal

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -1,5 +1,5 @@
 set fish_user_paths ~/bin
-set -e fish_greeting
+set fish_greeting
 set fish_prompt_pwd_dir_length 0
 if test -f ~/config-local/config/fish/config.fish
   source ~/config-local/config/fish/config.fish
