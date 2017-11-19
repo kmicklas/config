48c26faf1952550a2c540e6c486e938b5ddb5413
Ken Micklas <kmicklas@gmail.com>
2017-11-18 19:54:10 -0500

    Use XMonad's KDE config and switch to termite for terminal

--- dotfiles/xmonad/xmonad.hs
+++ dotfiles/xmonad/xmonad.hs
@@ -1,14 +1,15 @@
 import XMonad
+import XMonad.Config.Kde
 import XMonad.Hooks.Script
 import XMonad.Layout.NoBorders
 import XMonad.Util.EZConfig
 import XMonad.Util.Run
 
-main = xmonad $ defaultConfig
+main = xmonad $ kdeConfig
          { modMask = mod4Mask 
          , startupHook = execScriptHook "startup"
          , layoutHook = smartBorders $ layoutHook defaultConfig
-         , terminal = "gnome-terminal"
+         , terminal = "termite"
          } `additionalKeys`
          [ ((mod4Mask, xK_End), safeSpawn "xscreensaver-command" ["-lock"])
          , ((mod4Mask, xK_Page_Down), safeSpawn "systemctl" ["suspend"])
