dec10adb94a1d67fdd7b697ea72e9fd18c79a96e
Ken Micklas <kmicklas@gmail.com>
2017-11-25 18:55:53 -0500

    Attempt to fix KDE notifications in XMonad with probably overly permissive float classes

--- dotfiles/xmonad/xmonad.hs
+++ dotfiles/xmonad/xmonad.hs
@@ -2,8 +2,12 @@ import XMonad
 import XMonad.Config.Kde
 import XMonad.Layout.NoBorders
 
+myManageHook = composeAll [ className  =? c --> doFloat | c <- floatClasses ]
+   where floatClasses = ["Plasma", "Plasma-desktop", "plasma", "plasma-desktop", "plasmadesktop", "plasmashell"]
+
 main = xmonad $ kdeConfig
          { modMask = mod4Mask 
          , layoutHook = smartBorders $ layoutHook kdeConfig
+         , manageHook = manageHook kdeConfig <+> myManageHook
          , terminal = "termite"
          }
