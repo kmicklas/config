22c6d509d639063e47587d90542a679872a435aa
Ken Micklas <kmicklas@gmail.com>
2017-11-25 00:04:03 -0500

    Remove unused XMonad stuff

--- dotfiles/xmonad/xmonad.hs
+++ dotfiles/xmonad/xmonad.hs
@@ -1,14 +1,9 @@
 import XMonad
 import XMonad.Config.Kde
 import XMonad.Layout.NoBorders
-import XMonad.Util.EZConfig
-import XMonad.Util.Run
 
 main = xmonad $ kdeConfig
          { modMask = mod4Mask 
          , layoutHook = smartBorders $ layoutHook kdeConfig
          , terminal = "termite"
-         } `additionalKeys`
-         [ ((mod4Mask, xK_End), safeSpawn "xscreensaver-command" ["-lock"])
-         , ((mod4Mask, xK_Page_Down), safeSpawn "systemctl" ["suspend"])
-         ]
+         }
