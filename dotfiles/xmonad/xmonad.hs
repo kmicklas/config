7c3666313284c3b3079de2cedcd849e21097bc75
Ken Micklas <kmicklas@gmail.com>
2017-12-15 15:51:57 -0500

    Remove KDE stuff in XMonad config

--- dotfiles/xmonad/xmonad.hs
+++ dotfiles/xmonad/xmonad.hs
@@ -1,13 +1,8 @@
 import XMonad
-import XMonad.Config.Kde
 import XMonad.Layout.NoBorders
 
-myManageHook = composeAll [ className  =? c --> doIgnore | c <- floatClasses ]
-   where floatClasses = ["Plasma", "Plasma-desktop", "plasma", "plasma-desktop", "plasmadesktop", "plasmashell"]
-
-main = xmonad $ kdeConfig
+main = xmonad $ defaultConfig
          { modMask = mod4Mask 
-         , layoutHook = smartBorders $ layoutHook kdeConfig
-         , manageHook = manageHook kdeConfig <+> myManageHook
+         , layoutHook = smartBorders $ layoutHook defaultConfig
          , terminal = "termite"
          }
