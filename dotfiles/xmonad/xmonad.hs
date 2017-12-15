137decd29145dbac1f23c3904350054b5e188fc6
Ken Micklas <kmicklas@gmail.com>
2017-12-15 16:10:14 -0500

    Add three column XMonad layout

--- dotfiles/xmonad/xmonad.hs
+++ dotfiles/xmonad/xmonad.hs
@@ -1,8 +1,10 @@
 import XMonad
 import XMonad.Layout.NoBorders
+import XMonad.Layout.ThreeColumns
 
 main = xmonad $ defaultConfig
-         { modMask = mod4Mask 
-         , layoutHook = smartBorders $ layoutHook defaultConfig
+         { modMask = mod4Mask
+         , layoutHook = smartBorders $
+             ThreeColMid 1 (3/100) (1/2) ||| layoutHook defaultConfig
          , terminal = "termite"
          }
