3fc8e71a30a030a74ffbedfd4b53ea3348e858c5
Ken Micklas <kmicklas@gmail.com>
2017-12-24 15:22:48 -0500

    Remove deprecated use of `defaultConfig`

--- dotfiles/xmonad/xmonad.hs
+++ dotfiles/xmonad/xmonad.hs
@@ -1,9 +1,9 @@
 import XMonad
 import XMonad.Layout.ThreeColumns
 
-main = xmonad $ defaultConfig
+main = xmonad $ def
          { modMask = mod4Mask
-         , layoutHook = ThreeColMid 1 (3/100) (1/2) ||| layoutHook defaultConfig
+         , layoutHook = ThreeColMid 1 (3/100) (1/2) ||| layoutHook def
          , borderWidth = 0
          , terminal = "termite"
          }
