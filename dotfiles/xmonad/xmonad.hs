c10938038ccf98e106731b461ef86a8401106918
Ken Micklas <kmicklas@gmail.com>
2018-04-10 09:56:18 -0400

    Add workspace 0 to XMonad

--- dotfiles/xmonad/xmonad.hs
+++ dotfiles/xmonad/xmonad.hs
@@ -1,9 +1,23 @@
 import XMonad
 import XMonad.Layout.ThreeColumns
 
+import qualified Data.Map as M
+import qualified XMonad.StackSet as W
+
+myWorkspaces = pure <$> ['0' .. '9']
+myWorkspaceKeys = [xK_0 .. xK_9]
+
+myKeys conf@(XConfig {modMask = modm}) = M.fromList $
+  [ ((m .|. modm, k), windows $ f i)
+  | (i, k) <- zip myWorkspaces myWorkspaceKeys
+  , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
+  ]
+
 main = xmonad $ def
          { modMask = mod4Mask
          , layoutHook = ThreeColMid 1 (3/100) (1/2) ||| layoutHook def
          , borderWidth = 0
          , terminal = "termite"
+         , workspaces = myWorkspaces
+         , keys = myKeys <+> keys def
          }
