d720c7d521e83e366df997e64143d02fe49e9c83
Ken Micklas <kmicklas@gmail.com>
2019-04-30 16:02:05 -0400

    Use default instead of hard-coding cache.nixos.org

--- nix/modules/common/reflex.nix
+++ nix/modules/common/reflex.nix
@@ -1,6 +1,10 @@
-{ config, pkgs, ... }:
+{ config, options, pkgs, ... }:
 
 {
-  nix.binaryCaches = [ "https://cache.nixos.org" "https://nixcache.reflex-frp.org" ];
-  nix.binaryCachePublicKeys = [ "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI=" ];
+  nix.binaryCaches = options.nix.binaryCaches.default ++ [
+    "https://nixcache.reflex-frp.org"
+  ];
+  nix.binaryCachePublicKeys = [
+    "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI="
+  ];
 }
