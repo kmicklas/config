86da55a42923e3d0ad681864773c76a544d51e90
Ken Micklas <kmicklas@gmail.com>
2019-05-03 21:57:48 -0400

    Use home-manager for git config

--- nix/modules/common/kmicklas/home.nix
+++ nix/modules/common/kmicklas/home.nix
@@ -1,3 +1,20 @@
 { ... }:
 
-{}
+{
+  programs.git.enable = true;
+  programs.git.userName = "Ken Micklas";
+  programs.git.userEmail = "kmicklas@gmail.com";
+  programs.git.extraConfig = {
+    mergeTool.keepBackup = false;
+    push.default = "simple";
+    # TODO: Auto-generate these.
+    "url \"ssh://git@github.com\"".insteadOf = "https://github.com";
+    "url \"ssh://git@gitlab.com\"".insteadOf = "https://gitlab.com";
+    "url \"ssh://git@bitbucket.com\"".insteadOf = "https://bitbucket.com";
+  };
+  programs.git.ignores = [
+    "*~"
+    "cabal.project.local"
+    "dist-newstyle"
+  ];
+}
