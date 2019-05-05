e5707ef00773edc6e8b9efdc912cbbf6e2b127e1
Ken Micklas <kmicklas@gmail.com>
2019-05-05 17:12:21 -0400

    Add some XMonad hotkeys

--- dotfiles/xmonad/xmonad.hs
+++ dotfiles/xmonad/xmonad.hs
@@ -7,10 +7,17 @@ import qualified XMonad.StackSet as W
 myWorkspaces = pure <$> ['0' .. '9']
 myWorkspaceKeys = [xK_0 .. xK_9]
 
-myKeys conf@(XConfig {modMask = modm}) = M.fromList $
+workspace0Keys conf@(XConfig {modMask = modm}) = M.fromList $
   [ ((m .|. modm, k), windows $ f i)
   | (i, k) <- zip myWorkspaces myWorkspaceKeys
   , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
+  ]
+
+launcherKeys conf@(XConfig {modMask = modm}) = M.fromList $
+  [ ((modm, xK_a), spawn "autorandr -c")
+  , ((modm, xK_s), spawn "xscreensaver-command -lock; systemctl suspend")
+  , ((modm, xK_d), spawn "xscreensaver-command -lock")
+  , ((modm, xK_f), spawn "emacseditor")
   ]
 
 main = xmonad $ def
@@ -19,5 +26,5 @@ main = xmonad $ def
   , borderWidth = 0
   , terminal = "termite"
   , workspaces = myWorkspaces
-  , keys = myKeys <+> keys def
+  , keys = workspace0Keys <+> launcherKeys <+> keys def
   }
