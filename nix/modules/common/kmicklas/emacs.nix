8ea8fc0462f9b9de9c3cc4ba697cbe351e6f51e6
Ken Micklas <kmicklas@gmail.com>
2019-05-17 19:00:58 -0400

    Nixify spacemacs installation

--- nix/modules/common/kmicklas/emacs.nix
+++ nix/modules/common/kmicklas/emacs.nix
@@ -5,5 +5,11 @@
   services.emacs.enable = true;
   home.sessionVariables.EDITOR = "emacsclient --create-frame --tty";
   home.sessionVariables.VISUAL = "emacsclient --create-frame";
+
   home.file.".spacemacs".source = ../../../../dotfiles/spacemacs;
+  home.file.".emacs.d/spacemacs".source = ../../../../dep/spacemacs;
+  home.file.".emacs.d/init.el".text = ''
+    (setq spacemacs-start-directory "~/.emacs.d/spacemacs/")
+    (load-file (concat spacemacs-start-directory "init.el"))
+  '';
 }
