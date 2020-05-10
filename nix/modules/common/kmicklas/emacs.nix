1a9955972512cab147d782700630d17c0fa3f77c
Ken Micklas <git@kmicklas.com>
2020-05-10 18:48:15 -0400

    Start vanilla emacs configuration

--- nix/modules/common/kmicklas/emacs.nix
+++ nix/modules/common/kmicklas/emacs.nix
@@ -5,16 +5,30 @@
 let pkgs = import <nixpkgs> {};
 
 in {
+  imports = [
+    (import ../../../../dep/rycee-nur {}).hmModules.emacs-init
+  ];
+
   programs.emacs.enable = true;
   home.sessionVariables.EDITOR = "emacsclient --create-frame --tty";
   home.sessionVariables.VISUAL = "emacsclient --create-frame";
 
   home.file.".spacemacs".source = ../../../../dotfiles/spacemacs;
   home.file.".emacs.d/spacemacs".source = ../../../../dep/spacemacs;
-  home.file.".emacs.d/init.el".text = ''
+  home.file.".emacs.d/spacemacs.el".text = ''
     (setq spacemacs-start-directory "~/.emacs.d/spacemacs/")
     (load-file (concat spacemacs-start-directory "init.el"))
   '';
+
+  programs.emacs.init = {
+    enable = true;
+
+    usePackage = {
+      haskell-mode = {
+        enable = true;
+      };
+    };
+  };
 } // lib.optionalAttrs (!pkgs.stdenv.isDarwin) {
   services.emacs.enable = true;
 }
