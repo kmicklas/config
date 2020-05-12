2013929cfdfc3c127ec071eb3ea94efe7ec7bd0c
Ken Micklas <git@kmicklas.com>
2020-05-12 09:03:40 -0400

    Set up modifier keys on Mac OS

--- nix/modules/common/kmicklas/emacs/prelude.el
+++ nix/modules/common/kmicklas/emacs/prelude.el
@@ -10,6 +10,12 @@
 
 (when (not window-system) (xterm-mouse-mode 1))
 
+;; TODO: Optionally interpolate this with nix.
+(when (eq system-type 'darwin)
+ (setq mac-option-modifier 'alt)
+ (setq mac-command-modifier 'control)
+ )
+
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (menu-bar-mode -1)
