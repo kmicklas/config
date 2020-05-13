ffe29b8364d0dbfaa0c6479f50f0b2b2c14907cf
Ken Micklas <git@kmicklas.com>
2020-05-13 08:17:58 -0400

    Add more prelude defaults

--- nix/modules/common/kmicklas/emacs/prelude.el
+++ nix/modules/common/kmicklas/emacs/prelude.el
@@ -9,9 +9,13 @@
  )
 
 (setq-default
+ indent-tabs-mode nil
  tab-width 2
  c-basic-offset 2
+ show-trailing-whitespace t
  )
+
+(prefer-coding-system 'utf-8)
 
 (add-to-list 'default-frame-alist '(font . "Source Code Pro 8"))
 
@@ -28,3 +32,7 @@
 (menu-bar-mode -1)
 (blink-cursor-mode 0)
 (global-display-line-numbers-mode)
+(column-number-mode)
+(global-hl-line-mode 1)
+
+(defalias 'yes-or-no-p 'y-or-n-p)
