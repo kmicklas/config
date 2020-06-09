39ffb35f83cae2480c1243960ea15dd0697a2fbe
Ken Micklas <git@kmicklas.com>
2020-06-09 10:01:49 -0400

    Update oh-my-zsh so direnv plugin exists

--- nix/modules/common/kmicklas/zsh.nix
+++ nix/modules/common/kmicklas/zsh.nix
@@ -1,6 +1,15 @@
 { ... }:
 
 {
+  nixpkgs.overlays = [
+    (_: super: {
+      # TODO: Remove this once oh-my-zsh is upgraded in nixpkgs.
+      oh-my-zsh = super.oh-my-zsh.overrideAttrs (_: {
+        src = import ../../../../dep/ohmyzsh/thunk.nix;
+      });
+    })
+  ];
+
   programs.zsh.enable = true;
   programs.zsh.enableAutosuggestions = true;
   programs.zsh.oh-my-zsh.enable = true;
