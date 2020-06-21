265eb7444ff9cf07eb934a37c97c0425d6bb1f10
Ken Micklas <git@kmicklas.com>
2020-06-21 13:17:54 -0400

    Add more fonts

--- nix/modules/graphical/fonts.nix
+++ nix/modules/graphical/fonts.nix
@@ -7,9 +7,13 @@ pkgs: {
     unifont
     google-fonts
     noto-fonts
+    noto-fonts-cjk
+    noto-fonts-emoji
     source-han-sans-japanese
     source-han-sans-korean
     source-han-sans-simplified-chinese
     source-han-sans-traditional-chinese
+    wqy_microhei
+    wqy_zenhei
   ];
 }
