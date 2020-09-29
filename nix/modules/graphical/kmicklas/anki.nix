63e6ae1ef34b73b88b8184581256d9c49049a3da
Ken Micklas <git@kmicklas.com>
2020-09-28 21:03:40 -0400

    Upgrade anki to 2.1.22 with binary distribution

---
+++ nix/modules/graphical/kmicklas/anki.nix
@@ -0,0 +1,54 @@
+{ pkgs, ... }:
+
+{
+  nixpkgs.overlays = [
+    (self: super: {
+      anki = let
+        version = "2.1.22";
+        dist =
+          (self.stdenv.mkDerivation {
+            name = "anki-dist";
+			      src = self.fetchzip {
+			      	url = "https://github.com/ankitects/anki/releases/download/${version}/anki-${version}-linux-amd64.tar.bz2";
+			      	sha256 = "02a6jz3aj46v9vy7jbdkhj7hx8m8s6w2wpjrbz99w9bmx5px51x3";
+			      };
+            buildCommand = "cp -r $src $out";
+          });
+      in self.buildFHSUserEnv {
+        name = super.anki.name;
+        passthru = {
+          inherit (super.anki) meta man;
+          inherit version;
+        };
+
+        targetPkgs = pkgs: with pkgs; [
+          expat
+          fontconfig
+          freetype
+          glib
+          libGL
+          libxkbcommon
+          nspr
+          nss
+          systemd
+          zlib
+
+          xorg.libX11
+          xorg.libXcomposite
+          xorg.libXcursor
+          xorg.libXdamage
+          xorg.libXfixes
+          xorg.libXi
+          xorg.libXrandr
+          xorg.libXrender
+          xorg.libxcb
+        ];
+
+        runScript = "${dist}/bin/anki";
+        extraInstallCommands = ''
+          ls -l $out/bin
+        '';
+      };
+    })
+  ];
+}
--- nix/modules/graphical/kmicklas/home.nix
+++ nix/modules/graphical/kmicklas/home.nix
@@ -2,6 +2,7 @@
 
 {
   imports = [
+    ./anki.nix
     ./ao.nix
   ];
 
