bddf8e724fcca2011ff96d7e29493d9d1a288590
Ken Micklas <git@kmicklas.com>
2020-11-29 19:33:22 -0500

    Remove pass

--- nix/modules/common/kmicklas/home.nix
+++ nix/modules/common/kmicklas/home.nix
@@ -8,7 +8,6 @@
     ./rust.nix
     ./home-manager.nix
     ./nixpkgs.nix
-    ./pass.nix
     ./zsh.nix
   ] ++ lib.optionals (builtins.pathExists ../../../../private/nix/home/common) [
     ../../../../private/nix/home/common
