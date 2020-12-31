764ef6cf50fefa89c512da4f17b291a0c06ee61a
Ken Micklas <git@kmicklas.com>
2020-12-31 16:07:59 -0500

    Fix indentation in Anki derivation

--- nix/modules/graphical/kmicklas/anki.nix
+++ nix/modules/graphical/kmicklas/anki.nix
@@ -5,15 +5,14 @@
     (self: super: {
       anki = let
         version = "2.1.22";
-        dist =
-          (self.stdenv.mkDerivation {
-            name = "anki-dist";
-			      src = self.fetchzip {
-			      	url = "https://github.com/ankitects/anki/releases/download/${version}/anki-${version}-linux-amd64.tar.bz2";
-			      	sha256 = "02a6jz3aj46v9vy7jbdkhj7hx8m8s6w2wpjrbz99w9bmx5px51x3";
-			      };
-            buildCommand = "cp -r $src $out";
-          });
+        dist = self.stdenv.mkDerivation {
+          name = "anki-dist";
+          src = self.fetchzip {
+            url = "https://github.com/ankitects/anki/releases/download/${version}/anki-${version}-linux-amd64.tar.bz2";
+            sha256 = "02a6jz3aj46v9vy7jbdkhj7hx8m8s6w2wpjrbz99w9bmx5px51x3";
+          };
+          buildCommand = "cp -r $src $out";
+        };
       in self.buildFHSUserEnv {
         name = super.anki.pname;
         inherit (super.anki) meta;
