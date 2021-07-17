efe6be31bfbc92f02824eb26a8d0065c384b8e62
Ken Micklas <git@kmicklas.com>
2021-07-17 10:02:17 -0400

    Add root filesystem mount for Raspberry Pi SD card

--- nix/hosts/kjuk/default.nix
+++ nix/hosts/kjuk/default.nix
@@ -7,6 +7,14 @@
     "${fetchTarball "https://github.com/NixOS/nixos-hardware/archive/41775780a0b6b32b3d32dcc32bb9bc6df809062d.tar.gz"}/raspberry-pi/4"
   ];
 
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-label/NIXOS_SD";
+      fsType = "ext4";
+      options = [ "noatime" ];
+    };
+  };
+
   swapDevices = [ ];
 
   networking.hostName = "kjuk";
