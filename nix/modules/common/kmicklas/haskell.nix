1bdc6df419da61f768658cd0912bfb593d981f2e
Ken Micklas <git@kmicklas.com>
2021-10-08 19:32:05 -0400

    Get haskell-language-server from 21.05 now that it's fixed

--- nix/modules/common/kmicklas/haskell.nix
+++ nix/modules/common/kmicklas/haskell.nix
@@ -31,9 +31,8 @@
       lsp-haskell = {
         after = [ "lsp-mode" ];
         enable = true;
-        # TODO: Get haskell-language-server from 21.05 instead of master once it's unbroken again.
         config = ''
-          (setq lsp-haskell-server-path "${(import ../../../../dep/nixpkgs-master {}).haskell-language-server}/bin/haskell-language-server")
+          (setq lsp-haskell-server-path "${pkgs.haskell-language-server}/bin/haskell-language-server")
         '';
       };
     };
