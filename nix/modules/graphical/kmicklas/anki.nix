290b3858e00f7bb6008dcf828c9d78d12a555782
Ken Micklas <git@kmicklas.com>
2021-11-20 10:07:38 -0500

    Remove useless wrapper derivation for Anki source

--- nix/modules/graphical/kmicklas/anki.nix
+++ nix/modules/graphical/kmicklas/anki.nix
@@ -5,13 +5,9 @@
     (self: super: {
       anki = let
         version = "2.1.35";
-        dist = self.stdenv.mkDerivation {
-          name = "anki-dist";
-          src = self.fetchzip {
-            url = "https://github.com/ankitects/anki/releases/download/${version}/anki-${version}-linux-amd64.tar.bz2";
-            sha256 = "0kzblbb0hzy7hp8141rjyg68020d6kgbjdzsm632ng5y5g5i2f29";
-          };
-          buildCommand = "cp -r $src $out";
+        src = self.fetchzip {
+          url = "https://github.com/ankitects/anki/releases/download/${version}/anki-${version}-linux-amd64.tar.bz2";
+          sha256 = "0kzblbb0hzy7hp8141rjyg68020d6kgbjdzsm632ng5y5g5i2f29";
         };
       in self.buildFHSUserEnv {
         name = super.anki.pname;
@@ -50,7 +46,7 @@
           xorg.xcbutilwm
         ];
 
-        runScript = "${dist}/bin/anki";
+        runScript = "${src}/bin/anki";
       };
     })
   ];
