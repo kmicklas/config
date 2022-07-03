4741d98835c60f93a93b54d7161e91c9116ad49e
Ken Micklas <git@kmicklas.com>
2022-07-03 13:54:14 +0100

    Get Anki working on NixOS 22.05 with hacks

--- nix/modules/graphical/kmicklas/anki.nix
+++ nix/modules/graphical/kmicklas/anki.nix
@@ -9,7 +9,7 @@
           url = "https://github.com/ankitects/anki/releases/download/${version}/anki-${version}-linux-amd64.tar.bz2";
           sha256 = "0kzblbb0hzy7hp8141rjyg68020d6kgbjdzsm632ng5y5g5i2f29";
         };
-      in self.buildFHSUserEnv {
+      in (import ../../../../dep/nixpkgs-21.11 {}).buildFHSUserEnv {
         name = super.anki.pname;
         inherit (super.anki) meta;
 
@@ -46,14 +46,22 @@
           xorg.xcbutilwm
         ];
 
-        runScript = "${src}/bin/anki";
+        runScript = "anki-wrapped";
 
         extraInstallCommands = ''
           mkdir -p $out/share/applications
           mkdir -p $out/share/pixmaps
           cp ${src}/anki.desktop $out/share/applications
           cp ${src}/anki.png ${src}/anki.xpm $out/share/pixmaps
-          substituteInPlace $out/share/applications/anki.desktop --replace Exec=anki Exec=$out/bin/anki
+
+          cat << EOF > $out/bin/anki-wrapped
+          #!/bin/sh
+          QT_XCB_GL_INTEGRATION=none exec ${src}/bin/anki $@
+          EOF
+
+          chmod +x $out/bin/anki-wrapped
+
+          substituteInPlace $out/share/applications/anki.desktop --replace Exec=anki Exec=$out/bin/anki-wrapped
         '';
       };
     })
