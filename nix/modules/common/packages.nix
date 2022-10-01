0d76766d2d1349f9af5743bca0b3f3ec3a4b4f97
Ken Micklas <git@kmicklas.com>
2022-10-01 08:39:09 +0100

    Sort global packages

--- nix/modules/common/packages.nix
+++ nix/modules/common/packages.nix
@@ -1,19 +1,18 @@
 pkgs: with pkgs; [
-  wget
-  neovim
-  python
-  unzip
-  zstd
-  tmux
   alacritty.terminfo
-  silver-searcher
-  jq
-  gnupg
-  file
-
-  iotop
-  htop
-  ncdu
-  psmisc # killall
   exfat
+  file
+  gnupg
+  htop
+  iotop
+  jq
+  ncdu
+  neovim
+  psmisc # killall
+  python
+  silver-searcher
+  tmux
+  unzip
+  wget
+  zstd
 ]
