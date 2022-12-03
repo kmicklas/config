1f1d232c0b149980d81744cf1f13b26a57928090
Ken Micklas <git@kmicklas.com>
2022-12-03 11:26:47 +0000

    No need for unstable mpvacious now

--- user/graphical/mpv.nix
+++ user/graphical/mpv.nix
@@ -12,7 +12,7 @@
     sub-font-size = "50";
   };
   programs.mpv.scripts = [
-    (pkgs.callPackage "${import ../../dep/nixpkgs-unstable/thunk.nix}/pkgs/applications/video/mpv/scripts/mpvacious.nix" {})
+    pkgs.mpvScripts.mpvacious
   ] ++ lib.flip map ["sub-pause" "sub-skip"] (script:
     pkgs.stdenv.mkDerivation rec {
       pname = script;
