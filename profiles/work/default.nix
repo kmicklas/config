ed5e56b3685aed02bf9b53fb191c647a3e01e0ca
Ken Micklas <git@kmicklas.com>
2022-12-13 12:08:32 +0000

    Add some Emacs packages to work profile

--- profiles/work/default.nix
+++ profiles/work/default.nix
@@ -12,6 +12,19 @@
     "url \"https://github.com\"".insteadOf = "ssh://git@github.com";
   };
 
+  programs.emacs.init = {
+    usePackage = {
+      groovy-mode = {
+        enable = true;
+      };
+
+      lsp-pyright = {
+        enable = true;
+        after = [ "lsp-mode" ];
+      };
+    };
+  };
+
   home.packages = with pkgs; [
     dive
     go_1_18
