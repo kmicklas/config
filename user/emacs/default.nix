0498fe00aca079bc554fbf30f0305dc84cb37413
Ken Micklas <git@kmicklas.com>
2022-12-13 12:06:23 +0000

    Emacs native compilation is default now

--- user/emacs/default.nix
+++ user/emacs/default.nix
@@ -12,7 +12,6 @@ in {
   programs.emacs.enable = true;
   # TODO: Use emacs-overlay again if switching to Wayland and unstable doesn't
   # have pgtk yet.
-  programs.emacs.package = pkgs.emacsNativeComp;
 
   home.sessionVariables.EDITOR = "${config.programs.emacs.package}/bin/emacsclient --create-frame --tty";
   home.sessionVariables.VISUAL = "${config.programs.emacs.package}/bin/emacsclient --create-frame";
