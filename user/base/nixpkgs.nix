c333730dce63457bde0a2235aed5aafbccaff8dd
Ken Micklas <git@kmicklas.com>
2023-03-18 18:06:12 +0000

    Enable lorri service

--- user/base/nixpkgs.nix
+++ user/base/nixpkgs.nix
@@ -8,6 +8,8 @@ in
   nixpkgs.config = import nixpkgsConfig;
   xdg.configFile."nixpkgs/config.nix".source = nixpkgsConfig;
 
+  services.lorri.enable = true;
+
   home.packages = with pkgs; [
     cachix
     nix-tree
