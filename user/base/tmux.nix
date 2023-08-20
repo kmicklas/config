f796050f3c5ca839868630fb582e59f9bceee1e7
Ken Micklas <git@kmicklas.com>
2023-08-20 20:47:02 +0100

    Split tmux panes in current directory

--- user/base/tmux.nix
+++ user/base/tmux.nix
@@ -15,8 +15,8 @@
     bind Q kill-session
     bind r rename-window "#{b:pane_current_path}"
     bind R source-file ~/.config/tmux/tmux.conf
-    bind s split-window
-    bind v split-window -h
+    bind s split-window -c "#{pane_current_path}"
+    bind v split-window -h -c "#{pane_current_path}"
 
     bind -T root M-j select-pane -t :.+
     bind -T root M-k select-pane -t :.-
