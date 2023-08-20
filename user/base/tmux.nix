9916cb15f565d2e4d42c0c4f9578f844b9a1234d
Ken Micklas <git@kmicklas.com>
2023-08-20 17:03:35 +0100

    Attempt to fix tmux colors...

--- user/base/tmux.nix
+++ user/base/tmux.nix
@@ -6,7 +6,12 @@
   programs.tmux.historyLimit = 1000000;
   programs.tmux.mouse = true;
   programs.tmux.newSession = true;
+  programs.tmux.terminal = "tmux-256color";
+
   programs.tmux.extraConfig = ''
+    set -g default-terminal 'tmux-256color'
+    set -ga terminal-overrides ',xterm-256color:RGB'
+
     bind Q kill-session
     bind r rename-window "#{b:pane_current_path}"
     bind R source-file ~/.config/tmux/tmux.conf
