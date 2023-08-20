5c9be8c9c7b01ae4b1eb79a149ccc9ddc1662133
Ken Micklas <git@kmicklas.com>
2023-08-20 16:07:21 +0100

    Add tmux config

--- user/base/default.nix
+++ user/base/default.nix
@@ -4,6 +4,7 @@
   imports = [
     ./home-manager.nix
     ./nixpkgs.nix
+    ./tmux.nix
     ./zsh.nix
   ];
 
---
+++ user/base/tmux.nix
@@ -0,0 +1,19 @@
+{ ... }:
+
+{
+  programs.tmux.enable = true;
+
+  programs.tmux.historyLimit = 1000000;
+  programs.tmux.mouse = true;
+  programs.tmux.newSession = true;
+  programs.tmux.extraConfig = ''
+    bind Q kill-session
+    bind r rename-window "#{b:pane_current_path}"
+    bind R source-file ~/.config/tmux/tmux.conf
+    bind s split-window
+    bind v split-window -h
+
+    bind -T root M-j select-pane -t :.+
+    bind -T root M-k select-pane -t :.-
+  '';
+}
