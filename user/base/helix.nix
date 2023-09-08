8fda89674ac873a4b4b5190537b84823b670adc9
Ken Micklas <git@kmicklas.com>
2023-09-08 20:37:25 +0100

    Lots more helix config

--- user/base/helix.nix
+++ user/base/helix.nix
@@ -2,38 +2,48 @@
 
 let
   normal-keys = type: {
+    space.c = "toggle_comments";
     space.q.q = ":quit";
     space.q.a = ":quit-all";
     space.space = "command_palette";
     space.";" = "command_mode";
 
-    "C-g" = "collapse_selection";
-    "C-r" = "redo";
-    "C-s" = ":write";
-
     a = "open_below";
     A = "open_above";
     C-a = "open_above";
+    b = "rotate_view";
+    # TODO: C-b
     c = "keep_primary_selection";
+    C-c = "copy_selection_on_next_line";
     e = "append_mode";
     E = "insert_at_line_end";
     C-e = "insert_at_line_end";
     f = "insert_mode";
     F = "insert_at_line_start";
     C-f = "insert_at_line_start";
+    # TODO: C-g
     i = "${type}_next_word_start";
     I = "${type}_next_long_word_start";
+    C-k = "kill_to_line_end";
     o = "${type}_next_word_end";
     O = "${type}_next_long_word_end";
+    C-p = "paste_before";
+    C-r = "replace_selections_with_clipboard";
     s = "change_selection";
+    C-s = ":write";
+    C-t = "till_prev_char";
     u = "${type}_prev_word_start";
     U = "${type}_prev_long_word_start";
+    C-u = "redo";
     w = "undo";
+    # TODO: C-w
+    C-x = "extend_to_line_bounds";
+    # TODO: C-y
     "0" = "goto_line_start";
-    ";" = "goto_line_end";
     "," = "expand_selection";
-    # TODO: This doesn't seem to work for some reason.
-    # "." = "rotate_view";
+    "C-," = "unindent";
+    # TODO: . = jump mode
+    "C-." = "indent";
   };
 
 in {
@@ -41,6 +51,9 @@ in {
 
   programs.helix.settings = {
     theme = "github_dark";
+
+    editor.true-color = true;
+    editor.color-modes = true;
 
     keys.normal = normal-keys "move";
     keys.select = normal-keys "extend";
