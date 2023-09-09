e6220503e287684ed219164966f1344f77d09ac3
Ken Micklas <git@kmicklas.com>
2023-09-09 09:43:21 +0100

    Make tmux look nicer

--- user/base/tmux.nix
+++ user/base/tmux.nix
@@ -9,12 +9,20 @@
 
   programs.tmux.extraConfig = ''
     set -g allow-passthrough on # Allow Emacs clipetty to work
-    set -g default-terminal 'tmux-256color'
-    set -ga terminal-overrides ',xterm-256color:RGB'
+    set -g default-terminal "tmux-256color"
+    set -ga terminal-overrides ",xterm-256color:RGB"
 
     set -g renumber-windows on
     set -g base-index 1
     setw -g pane-base-index 1
+
+    set -g status-style "bg=#333333 fg=#bbbbbb dim"
+    set -g status-left ""
+    set -g status-right "#W #[fg=#cccccc,bg=#444444] %d/%m #[fg=#bbbbbb,bg=#333333] %H:%M "
+
+    setw -g window-status-separator ""
+    setw -g window-status-format "#[fg=#333333,bg=#99bbdd] #I #[fg=#cccccc,bg=#444444] #{b:pane_current_path} "
+    setw -g window-status-current-format "#[fg=#333333,bg=#ddbb99] #I #[fg=#cccccc,bg=#444444] #{b:pane_current_path} "
 
     unbind-key C-b
     set-option -g prefix C-Space
