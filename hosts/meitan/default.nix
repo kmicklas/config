696c317a4878575fbfc0c37f950de6b45bd75821
Ken Micklas <git@kmicklas.com>
2023-12-29 01:54:10 +0000

    Add new laptop

---
+++ hosts/meitan/default.nix
@@ -0,0 +1,28 @@
+{ ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+
+    ../../system/common
+    ../../system/efi-boot.nix
+    ../../system/graphical.nix
+    ../../system/kmicklas.nix
+    ../../system/libinput.nix
+    ../../system/media-keys.nix
+  ];
+
+  hardware.enableRedistributableFirmware = true;
+
+  boot.initrd.luks.devices = {
+    root = {
+      device = "/dev/disk/by-uuid/03f795b1-3449-44ed-992f-133c0691241d";
+      allowDiscards = true;
+    };
+  };
+
+  networking.hostName = "meitan";
+  networking.hostId = "96a6ca18";
+
+  nix.nixPath = [ ("nixos-config=" + builtins.toPath ./default.nix) ];
+}
