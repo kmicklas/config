19847d734177fcb251217739917545c3c0fd139f
Ken Micklas <git@kmicklas.com>
2024-04-03 13:55:30 +0100

    Remove unused Go tools (probably for Emacs go-mode)

--- user/go.nix
+++ user/go.nix
@@ -3,17 +3,10 @@
 {
   home.packages = with pkgs; [
     go
-    gocode
-    godef
     gofumpt
-    gogetdoc
     golangci-lint
     golint
-    gomodifytags
-    gopkgs
-    gotests
     gotools
-    impl
 
     gcc
   ];
