ae836c0650a7afcd9be516299635e3c7c32612ed
Ken Micklas <git@kmicklas.com>
2024-08-24 09:35:22 +0100

    Reduce boot configuration limit
    20 was still enough to fill up the boot partition.

--- system/efi-boot.nix
+++ system/efi-boot.nix
@@ -1,7 +1,7 @@
-{ config, pkgs, ... }:
+{ ... }:
 
 {
   boot.loader.systemd-boot.enable = true;
-  boot.loader.systemd-boot.configurationLimit = 20;
+  boot.loader.systemd-boot.configurationLimit = 10;
   boot.loader.efi.canTouchEfiVariables = true;
 }
