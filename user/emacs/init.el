ae53ffa0c07e0f2a4e1b07d6c421404a88bc8203
Ken Micklas <git@kmicklas.com>
2025-06-13 21:36:11 +0100

    emacs: import a bunch of old keybindings

--- user/emacs/init.el
+++ user/emacs/init.el
@@ -83,3 +83,113 @@
   :init
   (vertico-mode)
 )
+
+(use-package lsp-mode)
+
+(global-unset-key (kbd ","))
+
+(general-define-key
+  :prefix ","
+
+  "SPC" '((lambda () (interactive) (insert ", ")) :which-key ", ")
+  "," '(self-insert-command :which-key ",")
+  "." 'other-window
+
+  "a" '(org-agenda :which-key "agenda")
+
+  "b" '(:ignore t :which-key "buffer")
+  "bb" '(helm-mini :which-key "switch")
+  "bd" '(kill-this-buffer :which-key "delete")
+
+  "c" '(:ignore t :which-key "code")
+  "cc" '(comment-line :which-key "comment")
+  "cl" 'lsp
+  "cs" '(sort-lines :which-key "sort lines")
+  "ct" 'delete-trailing-whitespace
+
+  "d" '(dired-jump-other-window :which-key "dired other window")
+  "D" '(dired-jump :which-key "dired")
+
+  "e" '(:ignore t :which-key "eval")
+  "eb" '(eval-buffer :which-key "buffer")
+  "ee" '(eval-expression :which-key "expression")
+  "er" '(eval-region :which-key "region")
+
+  "f" '(:ignore t :which-key "file")
+  "fc" '(write-file :which-key "copy")
+  "fb" '(magit-blame :which-key "git blame")
+  "fe" '(revert-buffer-preserve-modes :which-key "revert")
+  "ff" '(find-file :which-key "find")
+  "fs" '(save-buffer :which-key "save")
+  "fy" 'copy-buffer-file-name
+
+  "h" '(:ignore t :which-key "help")
+  "hc" 'describe-char
+  "hf" 'describe-function
+  "hF" 'describe-face
+  "hk" 'describe-key
+  "hK" '(which-key-show-top-level :which-key "top level keys")
+  "hm" 'describe-mode
+  "hM" '(which-key-show-keymap :which-key "keymap")
+  "hp" 'describe-package
+  "hs" 'describe-symbol
+  "hv" 'describe-variable
+  "hy" '(yas-describe-tables :which-key "yasnippets")
+
+  "j" '(:ignore t :which-key "jump")
+  "jd" '(lsp-find-definition :which-key "definition")
+  "je" '(flycheck-next-error :which-key "error")
+  "jE" '(flycheck-previous-error :which-key "error")
+  "jf" '(beginning-of-defun :whick-key "function start")
+  "jF" '(end-of-defun :whick-key "function end")
+  "jl" '(evil-avy-goto-line :which-key "line")
+  "jr" '(lsp-find-references :which-key "references")
+  "jt" '(lsp-find-type-definition :which-key "type")
+
+  "i" '(:ignore t :which-key "insert")
+  "iu" 'uuidgen
+
+  "m" '(:ignore t :which-key "mode")
+
+  "n" '(:ignore t :which-key "narrow")
+  "nd" '(narrow-to-defun :which-key "defun")
+  "ns" '(org-narrow-to-subtree :which-key "org subtree")
+  "nw" 'widen
+
+  "g" '(magit-status :which-key "magit")
+
+  "l" '(:keymap lsp-command-map :package lsp-mode :which-key "language server")
+
+  "r" '(:ignore t :which-key "replace")
+  "rr" '(anzu-query-replace-regexp :which-key "regex")
+  "rs" '(anzu-query-replace :which-key "string")
+
+  "s" 'vterm-other-window
+  "S" 'vterm
+
+  "t"  '(:ignore t :which-key "toggle")
+  "tf" '(toggle-frame-fullscreen :which-key "fullscreen")
+  "ts" '(smartparens-mode :which-key "smartparens")
+
+  "v" '(:ignore t :which-key "view")
+  "vv" '(persp-switch :which-key "switch")
+  "vx" '(persp-kill :which-key "remove")
+
+  "w" '(:ignore t :which-key "window")
+  "wd" '(delete-window :which-key "delete")
+  "wm" '(popwin:messages :which-key "messages")
+  "wr" '(window-swap-states :whick-key "rotate")
+  "ws" 'split-window-vertically
+  "wv" 'split-window-horizontally
+  "ww" '(hydra-window-size/body :which-key "size")
+
+  "q" '(:ignore t :which-key "quit")
+  "qq" '(save-buffers-kill-terminal :which-key "window")
+)
+
+(general-define-key
+  "C-a" 'mark-whole-buffer
+  "C-f" 'isearch-forward
+  "C-s" 'save-buffer
+  "C-z" 'undo
+)
