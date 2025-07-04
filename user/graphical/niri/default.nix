c2ae36f4abd31760082ddc8cbf30c6936cffc083
Ken Micklas <git@kmicklas.com>
2025-07-03 21:17:36 -0700

    waybar: start setup for niri

--- user/graphical/niri/default.nix
+++ user/graphical/niri/default.nix
@@ -1,9 +1,17 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   # Niri enabled in system config for now.
+
+  xdg.portal.enable = true;
+  xdg.portal.config.common.default = "*";
+  xdg.portal.extraPortals = [
+    pkgs.xdg-desktop-portal-gtk
+  ];
+
   programs.fuzzel.enable = true;
   programs.swaylock.enable = true;
+  programs.waybar.enable = true;
 
   xdg.configFile."niri/config.kdl".source = ./config.kdl;
 }
