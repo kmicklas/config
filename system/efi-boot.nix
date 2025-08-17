4402f600b144ffeb385e03fbb43ff2ab849f3afa
Ken Micklas <git@kmicklas.com>
2025-08-17 10:39:51 +0100

    systemd-boot: disable command line editor as recommended

--- system/efi-boot.nix
+++ system/efi-boot.nix
@@ -2,6 +2,7 @@
 
 {
   boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.editor = false;
   boot.loader.systemd-boot.configurationLimit = 10;
   boot.loader.efi.canTouchEfiVariables = true;
 }
