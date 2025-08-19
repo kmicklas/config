3d261cd028a45623327f8e14ca4a57a900b716fd
Ken Micklas <git@kmicklas.com>
2025-08-19 19:36:55 +0100

    ghostty: set as default terminal

--- dotfiles/xmonad/xmonad.hs
+++ dotfiles/xmonad/xmonad.hs
@@ -39,7 +39,7 @@ main = xmonad $ ewmh $ docks $ def
   , layoutHook = avoidStruts $ lessBorders OtherIndicated $
       layoutHook def ||| ThreeColMid 1 (3/100) (1/2)
   , borderWidth = 5
-  , terminal = "alacritty"
+  , terminal = "ghostty"
   , workspaces = myWorkspaces
   , keys = workspace0Keys <+> launcherKeys <+> keys def
 
