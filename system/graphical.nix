f09a1b0ec4ff47b835ca32be9a034d87cf9e7b6d
Ken Micklas <git@kmicklas.com>
2026-01-18 18:18:07 +0000

    pipewire: enable and switch from pulse

--- system/graphical.nix
+++ system/graphical.nix
@@ -27,11 +27,11 @@
     };
   };
 
-  # TODO: switch to pipewire?
-  services.pipewire.enable = false;
-  services.pulseaudio.enable = true;
-  services.pulseaudio.package = pkgs.pulseaudioFull;
-  services.pulseaudio.daemon.config.flat-volumes = "no";
+  security.rtkit.enable = true;
+  services.pipewire.enable = true;
+  services.pipewire.alsa.enable = true;
+  services.pipewire.alsa.support32Bit = true;
+  services.pipewire.pulse.enable = true;
 
   networking.networkmanager.enable = true;
   networking.dhcpcd.enable = false;
