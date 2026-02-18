f439d9196ef2ecd6cbc72961d8e8c4dc291d2009
Ken Micklas <git@kmicklas.com>
2026-02-18 19:38:00 +0000

    jj: show diff in commit draft

--- user/jujutsu.nix
+++ user/jujutsu.nix
@@ -6,6 +6,18 @@
     user.name = "Ken Micklas";
 
     ui.default-command = "log";
+
+    templates.draft_commit_description = ''
+      concat(
+        description,
+        surround(
+          "\nJJ: This commit contains the following changes:\n", "",
+          indent("JJ:     ", diff.stat(72)),
+        ),
+        "\nJJ: ignore-rest\n",
+        diff.git(),
+      )
+    '';
   };
 
   programs.delta.enableJujutsuIntegration = true;
