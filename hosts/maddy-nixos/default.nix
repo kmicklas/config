8b76996bf62c260399b5bbfd09481d45c0eed697
Ken Micklas <git@kmicklas.com>
2026-04-04 19:38:15 +0100

    hosts/maddy-nixos: use common and EFI modules

--- hosts/maddy-nixos/default.nix
+++ hosts/maddy-nixos/default.nix
@@ -1,27 +1,21 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
-{ inputs, nixpkgsConfig, pkgs, ... }:
+{ inputs, pkgs, ... }:
 
 {
   ## DISK CONFIGURATION
 
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      inputs.nixos-hardware.nixosModules.dell-xps-13-9360
-    ];
+  imports = [
+    ./hardware-configuration.nix
+    inputs.nixos-hardware.nixosModules.dell-xps-13-9360
 
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+    ../../system/common
+    ../../system/efi-boot.nix
+  ];
+
   boot.supportedFilesystems = [ "ntfs" ];
 
   ## NETWORK
 
   networking.networkmanager.enable = true;
-
 
   ## LOCALIZATION
 
@@ -33,11 +27,7 @@
      inputMethod.ibus.engines = with pkgs.ibus-engines; [ anthy ];
    };
 
-  time.timeZone = "America/New_York";
-
-
   ## PACKAGES & ENV
-  programs.nix-ld.enable = true;
     
   # Steam not supported as a user package...
   programs.steam = {
@@ -49,17 +39,7 @@
   services.flatpak.enable = true;
   xdg.portal.enable = true;  
 
-  nixpkgs.config = nixpkgsConfig;
-
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
-
   nix.nixPath = [ ("nixos-config=" + builtins.toPath ./default.nix) ];
-
-  nix.binaryCaches = [ "https://nixcache.reflex-frp.org" ];
-  nix.binaryCachePublicKeys = [ "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI=" ];
 
   ## SERVICES
   services.physlock.enable = true;
@@ -128,12 +108,5 @@
    };
 
 
-  ## NIXOS
-
-  # This value determines the NixOS release with which your system is to be
-  # compatible, in order to avoid breaking some software such as database
-  # servers. You should change this only after NixOS release notes say you
-  # should.
-  system.stateVersion = "18.09"; # Did you read the comment?
-
+  system.stateVersion = "18.09";
 }
