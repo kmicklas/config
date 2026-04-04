9a7673fc11ec9d5f2f5389c5b6ddf549e0223803
Ken Micklas <git@kmicklas.com>
2026-04-04 08:59:58 +0100

    hosts/maddy-nixos: fix font package names

--- hosts/maddy-nixos/default.nix
+++ hosts/maddy-nixos/default.nix
@@ -94,14 +94,11 @@
     enableGhostscriptFonts = true;
     fonts = with pkgs; [
       corefonts
-      ubuntu_font_family
+      ubuntu-sans
       unifont
       google-fonts
       noto-fonts
-      source-han-sans-japanese
-      source-han-sans-korean
-      source-han-sans-simplified-chinese
-      source-han-sans-traditional-chinese
+      source-han-sans
     ];
   };
 
