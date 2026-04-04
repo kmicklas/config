a20a2938a8dad651800e875cde27f082ef483a7c
Ken Micklas <git@kmicklas.com>
2026-04-04 10:50:49 +0100

    flake: dynamically detect hosts/users

--- flake.nix
+++ flake.nix
@@ -30,22 +30,27 @@
 
   outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, home-manager, ... }:
     let
-      username = "kmicklas";
-      hosts = [
-        {
-          name = "meitan";
-          system = "x86_64-linux";
-        }
-        {
-          name = "pej";
-          system = "x86_64-linux";
-        }
-      ];
       nixpkgsPath = nixpkgs.outPath;
       nixpkgsConfig = {
         allowBroken = true;
         allowUnfree = true;
       };
+
+      listDirectories = path:
+        builtins.attrNames
+        (nixpkgs.lib.filterAttrs (_: type: type == "directory")
+          (builtins.readDir path));
+      optionalPath = path: if builtins.pathExists path then path else null;
+
+      hostRoot = ./hosts;
+      hostNames = listDirectories hostRoot;
+      hosts = map (name: {
+        inherit name;
+        system = import (hostRoot + "/${name}/system.nix");
+      }) hostNames;
+      hostUsers = host:
+        let usersPath = optionalPath (hostRoot + "/${host.name}/users");
+        in if usersPath == null then [ ] else listDirectories usersPath;
 
       mkPkgs = repo: system:
         import repo {
@@ -85,18 +90,28 @@
           modules = [ (./hosts + "/${name}/default.nix") ];
         };
 
+      mkHomeEntries = host: username:
+        let
+          homePath = optionalPath
+            (hostRoot + "/${host.name}/users/${username}/home.nix");
+        in if homePath == null then
+          [ ]
+        else [{
+          name = "${username}@${host.name}";
+          value = mkHome {
+            inherit (host) system;
+            modules = [ homePath ];
+          };
+        }];
+
     in {
       formatter = forAllSystems (pkgs: pkgs.nixfmt-classic);
 
       lib = { inherit mkHome; };
 
-      homeConfigurations = nixpkgs.lib.listToAttrs (map (host: {
-        name = "${username}@${host.name}";
-        value = mkHome {
-          inherit (host) system;
-          modules = [ (./hosts + "/${host.name}/users/${username}/home.nix") ];
-        };
-      }) hosts);
+      homeConfigurations = nixpkgs.lib.listToAttrs (nixpkgs.lib.concatMap
+        (host: nixpkgs.lib.concatMap (mkHomeEntries host) (hostUsers host))
+        hosts);
 
       nixosConfigurations = nixpkgs.lib.listToAttrs (map (host: {
         name = host.name;
