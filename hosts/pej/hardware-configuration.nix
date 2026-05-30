08feae5d767bff05d8798d607875b846416534be
Ken Micklas <git@kmicklas.com>
2026-05-30 17:12:25 +0100

    hosts/pej: regen-hardware-config

--- hosts/pej/hardware-configuration.nix
+++ hosts/pej/hardware-configuration.nix
@@ -8,7 +8,7 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
@@ -18,13 +18,13 @@
       fsType = "zfs";
     };
 
-  fileSystems."/home" =
-    { device = "zroot/nixos/home";
+  fileSystems."/nix" =
+    { device = "zroot/nixos/nix";
       fsType = "zfs";
     };
 
-  fileSystems."/nix" =
-    { device = "zroot/nixos/nix";
+  fileSystems."/home" =
+    { device = "zroot/nixos/home";
       fsType = "zfs";
     };
 
